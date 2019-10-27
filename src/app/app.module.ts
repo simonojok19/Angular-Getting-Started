e015ec39464d111d5feea125b2c201dd0bba7c8a
Simon Peter Ojok <simonojok19@gmail.com>
2019-10-27 14:08:43 +0300

    failed to add filter features, hard to understand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductsListComponent } from './products/products-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/products/products.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  releasedDate: string;
+  price: string;
+  description: string;
+  starRating: string;
+  imageUrl: string;
+}
